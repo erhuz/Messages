f6f47c5b5de1274e994adf0f900e2e6ecbb5857d
Benjamin Fransson <benjamin@cafcon.se>
2019-04-10 19:54:43 +0200

    Added basic view templates

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            <h1>
+                Hello {{config('app.name')}}!
+            </h1>
+        </div>
+    </div>
+@endsection
