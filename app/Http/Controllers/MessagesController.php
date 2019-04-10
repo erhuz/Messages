475aa5dd6f4139fa75080cb7635223161a1f86c9
Benjamin Fransson <benjamin@cafcon.se>
2019-04-10 20:46:38 +0200

    imported Messages model

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Messages;
 
 class MessagesController extends Controller
 {
     public function index()
     {
+
+
         return view('index');
     }
 }
